eb5f0f0861e9587d814cc7b00838037942e70a5c
Fahad Al Foysal <60594760+Al-foysal@users.noreply.github.com>
2021-01-05 12:11:36 +0600

    Add files via upload

---
+++ Repository/IProductTypeRepo.cs
@@ -0,0 +1,44 @@
+﻿using M7_Project_05.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace M7_Project_05.Repository
+{
+	interface IProductTypeRepo
+	{
+		List<Catagory> GetAll();
+		List<Product> GetProduct(int id);
+		List<Catagory> GetTypesForDropDown();
+		void InsertProductTypeWithProduct(ProductType pb);
+	}
+	public class ProductTypeRepo : IProductTypeRepo
+	{
+		bpDbContext db = new bpDbContext();
+		public List<Catagory> GetAll()
+		{
+			return db.Catagories
+		   .Include(t => t.ProductTypes.Select(eq => eq.Products)).ToList();
+
+		}
+
+		public List<Product> GetProduct(int id)
+		{
+			return db.Products.Where(x => x.ProductTypeId == id).ToList();
+		}
+
+		public List<Catagory> GetTypesForDropDown()
+		{
+			return db.Catagories.ToList();
+		}
+
+		public void InsertProductTypeWithProduct(ProductType pb)
+		{
+			db.ProductTypes.Add(pb);
+			db.SaveChanges();
+		}
+	}
+}
